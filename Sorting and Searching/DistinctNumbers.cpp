aea9cc187a1dbf1ed23017e0a3999f624ab26536
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-02-24 12:24:08 -0800

    Create DistinctNumbers.cpp

---
+++ Sorting and Searching/DistinctNumbers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+#define max(n, m) ((n>m)?n:m)
+#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+ 
+int nums[200010];
+int main()
+{
+    int n, ans = 1;
+    cin >> n;
+    for(int i = 0; i < n; i ++)
+        cin >> nums[i];
+    sort(nums, nums + n);
+    for(int i = 1; i < n; i ++)
+        if(nums[i] != nums[i - 1])
+            ans ++;
+    cout << ans << "\n";
+}
