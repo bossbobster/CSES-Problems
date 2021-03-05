3f2af415684c2dd13e44aeb2bf09c7ce4d471fd1
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-03-05 10:15:13 -0800

    I used bitmask DP (soln continued in description).
    For every m, a row was a bunch of bits, 1 for the top of a vertical (that will go to the next row), and 0 for anything else (that will only stay on the current row). This way, I could use the fact that all the 0s would not carry over to the next row to my advantage. I first calculated all of the possible next states for each current state, and even though this might initially seem to be 2^n, a lot of those states are eliminated, and it can be proved that the maximum amount of next states is the nth Fibonacci number (in practice, this ends up being an average of around 10-20 or so for this n). Overall, the complexity is O(m * 2^n * fib(n)), which is fast enough to pass.

---
+++ Dynamic Programming/CountingTilings.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+#define max(n, m) ((n>m)?n:m)
+#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+
+vector<vector<int>> fib[11];
+vector<int> nx[1050];
+vector<pii> small;
+vector<int> curNx;
+int dp[1010][1050];
+void findAll(vector<int> cur, int idx)
+{
+    if(idx == small.size())
+    {
+        int sum = 0, pw = 1;
+        for(int i = 0; i < cur.size(); i ++)
+        {
+            sum += cur[i] * pw;
+            pw *= 2;
+        }
+        curNx.push_back(sum);
+        return;
+    }
+    for(int i = 0; i < fib[small[idx].s].size(); i ++)
+    {
+        for(int j = 0; j < small[idx].s; j ++)
+            cur[j + small[idx].f] = fib[small[idx].s][i][j];
+        findAll(cur, idx + 1);
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> emp;
+    fib[0].push_back(emp); emp.push_back(1); fib[1].push_back(emp);
+    for(int i = 2; i <= n; i ++)
+    {
+        for(int j = 0; j < fib[i - 2].size(); j ++)
+        {
+            vector<int> tmp = fib[i - 2][j];
+            tmp.push_back(0); tmp.push_back(0);
+            fib[i].push_back(tmp);
+        }
+        for(int j = 0; j < fib[i - 1].size(); j ++)
+        {
+            vector<int> tmp = fib[i - 1][j];
+            tmp.push_back(1);
+            fib[i].push_back(tmp);
+        }
+    }
+    for(int i = 0; i < (1 << n); i ++)
+    {
+        vector<int> tmp, cur;
+        tmp.resize(n);
+        small.clear(); curNx.clear();
+        for(int j = 0; j < n; j ++)
+        {
+            if(((1 << j) & i)) cur.push_back(1);
+            else cur.push_back(0);
+        }
+        for(int j = 0; j < n; j ++)
+        {
+            if(cur[j] == 1)
+                tmp[j] = 0;
+            else
+                tmp[j] = 2;
+        }
+        pii pb = {0, 0};
+        for(int j = 0; j < n; j ++)
+        {
+            if(tmp[j] == 0)
+            {
+                if(pb.s > 0) small.push_back(pb);
+                pb.f = j + 1; pb.s = 0;
+            }
+            else
+                pb.s ++;
+        }
+        if(pb.s > 0) small.push_back(pb);
+        findAll(tmp, 0);
+        nx[i] = curNx;
+    }
+    for(int i = 0; i < fib[n].size(); i ++)
+    {
+        int sum = 0, pw = 1;
+        for(int j = 0; j < n; j ++)
+        {
+            sum += fib[n][i][j] * pw;
+            pw *= 2;
+        }
+        dp[0][sum] ++;
+    }
+    for(int i = 0; i < m - 1; i ++)
+        for(int j = 0; j < (1 << n); j ++)
+            for(int k = 0; k < nx[j].size(); k ++)
+                dp[i + 1][nx[j][k]] = (dp[i + 1][nx[j][k]] + dp[i][j]) % 1000000007;
+    cout << dp[m - 1][0] << "\n";
+    return 0;
+}
