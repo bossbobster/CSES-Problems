2d0c82ae367f44a233ac30112e577192d66ec1d4
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-04-17 10:35:23 -0700

    Someone hacked my original code, this one works better.

--- Sorting and Searching/SumOfTwoValues.cpp
+++ Sorting and Searching/SumOfTwoValues.cpp
@@ -1,45 +1,60 @@
 #include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdio.h>
 #include <iomanip>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
 #include <vector>
+#include <stack>
 #include <queue>
 #include <bitset>
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
 typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
 typedef pair<ll, ll> pll;
-#define max(n, m) ((n>m)?n:m)
-#define min(n, m) ((n<m)?n:m)
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
- 
-pii nums[200010];
+
+struct chash { // use most bits rather than just the lowest ones
+    const uint64_t C = ll(2e18*(3.14))+71; // large odd number
+    const int RANDOM = rand(); // random 32-bit number
+    ll operator()(ll x) const {
+        // https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+        return __builtin_bswap64((x^RANDOM)*C);
+    }
+};
+
+int nums[200010];
+gp_hash_table<int, int, chash> m;
 int main()
 {
     int n, target;
-    int other;
     cin >> n >> target;
-    if(n == 1) { cout << "IMPOSSIBLE\n"; return 0; }
+    for(int i = 0; i < n; i ++)
+        cin >> nums[i];
     for(int i = 0; i < n; i ++)
     {
-        cin >> nums[i].f;
-        nums[i].s = i;
-    }
-    sort(nums, nums + n);
-    for(int i = 0; i < n; i ++)
-    {
-        other = lower_bound(nums + i + 1, nums + n, pii(target - nums[i].f, 0)) - nums;
-        if(nums[other].f + nums[i].f == target)
+        if(m.find(target - nums[i]) != m.end())
         {
-            cout << nums[i].s + 1 << " " << nums[other].s + 1 << "\n";
+            cout << m[target - nums[i]]+1 << " " << i+1 << "\n";
             return 0;
         }
+        m[nums[i]] = i;
     }
-    cout << "IMPOSSIBLE" << "\n";
+    cout << "IMPOSSIBLE\n";
 }
