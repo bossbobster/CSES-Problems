30b28e1eb8d19c1c96fcd86d6ba425276a95463f
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-06-19 10:54:51 -0700

    Similar logic to nim game 1, but this time we must take each element (mod 4). The reason for this is because it is optimal to always end a move with a nim sum of 0, which is always possible if the XOR of all elements (mod 4) is not 0 to begin with.

---
+++ Mathematics/NimGameII.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int nums[200010];
+int main()
+{
+    input();
+    int t, n, ans;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        ans = 0;
+        for(int i = 0; i < n; i ++)
+            cin >> nums[i];
+        for(int i = 0; i < n; i ++)
+            ans ^= (nums[i]%4);
+        if(ans == 0) cout << "second\n";
+        else cout << "first\n";
+    }
+}
