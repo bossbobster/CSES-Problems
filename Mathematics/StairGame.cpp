cf1e3d02acd9389f8d7ed20f5c54b0364f95c96c
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-03 11:49:48 -0700

    Each move can be repeated by the other player (so moving m rocks from k to k-1 can be done again from k-1 to k-2). The XOR sum rule therefore only stays for alternating numbers.

---
+++ Mathematics/StairGame.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int main()
+{
+    input();
+    int t, n, num, ans;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        ans = 0;
+        for(int i = 0; i < n; i ++)
+        {
+            cin >> num;
+            if(i&1) ans ^= num;
+        }
+        cout << ((ans==0)?"second\n":"first\n");
+    }
+}
