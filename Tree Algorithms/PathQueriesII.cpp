f738c9f66e52c56d99b52414ab969fe0fde5d532
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-29 11:19:30 -0700

    Heavily improved memory to O(n) and also slightly faster time.

--- Tree Algorithms/PathQueriesII.cpp
+++ Tree Algorithms/PathQueriesII.cpp
@@ -18,6 +18,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <complex>
+#include <valarray>
 //#include <ext/pb_ds/assoc_container.hpp>
 //using namespace __gnu_pbds;
 using namespace std;
@@ -40,18 +41,17 @@ typedef complex<double> cd;
 #define s second
 #define input() ios_base::sync_with_stdio(0);cin.tie(0);
 
-int n, m, a, b, cnt = 0, type, lca, bitI = 0;
-int nums[400010];
-vector<int> adj[400010];
-int sz[400010];
-int chain[400010];
-int pars[400010];
-int idx[400010];
-int arr[400010], dep[400010];
-int bitIdx[400010];
-int chainSt[400010];
-vector<int> tree[100010];
-int chainSz[400010];
+int n, m, a, b, cnt = 0, type, bitI = 0;
+int nums[200010];
+vector<int> adj[200010];
+int sz[200010];
+int chain[200010];
+int pars[200010];
+int dep[200010];
+int bitIdx[200010];
+int chainSt[200010];
+vector<int> tree[200010];
+int chainSz[200010];
 void up(int which, int idx, int val)
 {
     tree[which][idx + chainSz[which]] = val;
@@ -71,13 +71,13 @@ int maxQ(int which, int l, int r) // inclusive l, exclusive r
     }
     return ans;
 }
-
 void dfs1(int cur, int par)
 {
     pars[cur] = par;
     for(auto it : adj[cur])
     {
         if(it == par) continue;
+        dep[it] = dep[cur]+1;
         dfs1(it, cur);
         sz[cur] += sz[it];
     }
@@ -88,7 +88,7 @@ void dfs2(int cur, int par)
     chain[cur] = cnt; chainSz[cnt] ++;
     bitIdx[cur] = bitI++;
     if(adj[cur].size() == 1 && adj[cur][0] == par) return;
-    int mx = 400005;
+    int mx = 200005;
     for(int i = 0; i < adj[cur].size(); i ++)
     {
         if(adj[cur][i] == par) continue;
@@ -101,46 +101,16 @@ void dfs2(int cur, int par)
         cnt++; chainSt[cnt] = adj[cur][i]; bitI = 0; dfs2(adj[cur][i], cur);
     }
 }
-void euler(int cur, int par, int d)
-{
-    dep[cnt] = d;
-    arr[cnt] = cur;
-    idx[cur] = cnt++;
-    for(auto it : adj[cur])
-        if(it != par)
-        {
-            euler(it, cur, d+1);
-            dep[cnt] = d;
-            arr[cnt++] = cur;
-        }
-}
-pii pre[400010][25];
-void sparse(int n)
-{
-    for(int i = 0; i < n; i ++)
-        pre[i][0] = {dep[i], i};
-    for(int i = 1; i <= floor(log2(n)); i ++)
-        for(int j = 0; j <= n - (1 << i); j ++)
-            pre[j][i] = min(pre[j][i - 1], pre[j + (1 << (i - 1))][i - 1]);
-    
-}
-int minQ(int l, int r)
-{
-    int log = floor(log2(r - l + 1));
-    if(pre[l][log].f < pre[r-(1<<log)+1][log].f) return arr[pre[l][log].s];
-    return arr[pre[r-(1<<log)+1][log].s];
-}
-int path(int st, int en) //st is up, en is down
+int path(int st, int en)
 {
     int ans = 0;
-    if(chain[st] == chain[en]) return maxQ(chain[en], bitIdx[st], bitIdx[en]+1);
-    ans = maxQ(chain[en], 0, bitIdx[en]+1);
-    en = pars[chainSt[chain[en]]];
     while(chain[st] != chain[en])
     {
+        if(dep[chainSt[chain[st]]] > dep[chainSt[chain[en]]]) swap(st, en);
         ans = max(ans, maxQ(chain[en], bitIdx[chainSt[chain[en]]], bitIdx[en]+1));
         en = pars[chainSt[chain[en]]];
     }
+    if(bitIdx[st] > bitIdx[en]) swap(st, en);
     ans = max(ans, maxQ(chain[en], bitIdx[st], bitIdx[en]+1));
     return ans;
 }
@@ -155,15 +125,12 @@ int main()
         cin >> a >> b; a--; b--;
         adj[a].push_back(b); adj[b].push_back(a);
     }
-    sz[400005] = -1;
+    sz[200005] = -1;
     dfs1(0, -1); dfs2(0, -1);
     for(int i = 0; i <= cnt; i ++)
-        tree[i].resize(chainSz[i]*4);
+        tree[i].resize(chainSz[i]*2);
     for(int i = 0; i < n; i ++)
         up(chain[i], bitIdx[i], nums[i]);
-    cnt = 0;
-    euler(0, -1, 0);
-    sparse(2*n);
     while(m--)
     {
         cin >> type >> a >> b;
@@ -176,8 +143,7 @@ int main()
         else
         {
             a--; b--;
-            lca = minQ(min(idx[a], idx[b]), max(idx[a], idx[b]));
-            cout << max(path(lca, a), path(lca, b)) << ((m==0)?"\n":" ");
+            cout << path(a, b) << ((m==0)?"\n":" ");
         }
     }
     return 0;
