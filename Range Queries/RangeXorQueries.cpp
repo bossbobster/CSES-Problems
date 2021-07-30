6b89bd01ffcd64132b2e43f4b6a1ec5155bdec64
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-30 16:38:05 -0700

    Way faster code.

--- Range Queries/RangeXorQueries.cpp
+++ Range Queries/RangeXorQueries.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <string.h>
+#include <random>
 #include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iomanip>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
 #include <vector>
 #include <stack>
 #include <queue>
@@ -13,57 +17,43 @@
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <complex>
+#include <valarray>
 //#include <ext/pb_ds/assoc_container.hpp>
 //using namespace __gnu_pbds;
 using namespace std;
 typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
 typedef pair<ll, ll> pll;
-#define max(n, m) ((n>m)?n:m)
-#define min(n, m) ((n<m)?n:m)
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
-
-int n;
-int tree[400010];
-int nums[200010];
-void makeTree(int nums[])
-{
-    for(int i = 0; i < n; i ++)
-        tree[n + i] = nums[i];
-    for(int i = n - 1; i > 0; -- i)
-        tree[i] = tree[i << 1] ^ tree[i << 1 | 1];
-}
-void update(int idx, int val)
-{
-    tree[idx + n] = val;
-    idx += n;
-    for(int i = idx; i > 1; i >>= 1)
-        tree[i >> 1] = tree[i] ^ tree[i ^ 1];
-}
-int minQ(int l, int r) // inclusive l, exclusive r
-{
-    int ans = 0;
-    for(l += n, r += n; l < r; l >>= 1, r >>= 1)
-    {
-        if(l & 1)
-            ans = ans ^ tree[l ++];
-        if(r & 1)
-            ans = ans ^ tree[-- r];
-    }
-    return ans;
-}
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+ 
+int pre[200010];
 int main()
 {
-    int q, a, b;
+    input();
+    int n, q, num, a, b;
     cin >> n >> q;
-    for(int i = 0; i < n; i ++)
-        cin >> nums[i];
-    makeTree(nums);
-    for(int i = 0; i < q; i ++)
+    pre[0] = 0;
+    for(int i = 1; i <= n; i ++)
+        cin >> num, pre[i] = pre[i-1]^num;
+    while(q--)
     {
         cin >> a >> b;
-        cout << minQ(a - 1, b) << "\n";
+        cout << (pre[b] ^ pre[a-1]) << "\n";
     }
 }
