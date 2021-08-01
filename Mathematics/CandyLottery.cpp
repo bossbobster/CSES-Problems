b5e5550a1eced776d42e6e458c5fd6e142f35041
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-07-31 22:41:05 -0700

    Find the probability that each number is the maximum, multiply each of those by the number and add to answer (answer will be expected max).

---
+++ Mathematics/CandyLottery.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iomanip>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <bitset>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <complex>
+#include <valarray>
+//#include <ext/pb_ds/assoc_container.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
+typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
+#define f first
+#define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+
+int main()
+{
+    input();
+    ld n, k, num1, num2, ans = 0;
+    cin >> n >> k;
+    for(ld i = 1; i <= k; i ++)
+    {
+        num1 = pow(i/k, n); num2 = pow((i-1)/k, n);
+        ans += i*(num1-num2);
+    }
+    cout << fixed << setprecision(6) << ans << "\n";
+}
