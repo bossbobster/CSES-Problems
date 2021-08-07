a0753881e84598ffe756db87c3d46dcae9e2862c
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-08-06 21:53:29 -0700

    I improved the runtime of the code.

--- Sorting and Searching/NestedRangesCheck.cpp
+++ Sorting and Searching/NestedRangesCheck.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iomanip>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
 #include <vector>
+#include <stack>
 #include <queue>
 #include <bitset>
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <complex>
+#include <valarray>
 #include <ext/pb_ds/assoc_container.hpp>
 using namespace __gnu_pbds;
 using namespace std;
 typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
 typedef pair<ll, ll> pll;
-#define max(n, m) ((n>m)?n:m)
-#define min(n, m) ((n<m)?n:m)
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
- 
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+struct chash
+{
+    const uint64_t C = ll(2e18*(3.14))+71;
+    const int RANDOM = rand();
+    ll operator()(ll x) const
+    {
+        return __builtin_bswap64((x^RANDOM)*C);
+    }
+};
+
 int sum(int arr[], int idx)
 {
     int ans = 0;
@@ -55,7 +87,7 @@ bool comp2(st s1, st s2)
     if(s1.a == s2.a) return s1.b > s2.b;
     return s1.a < s2.a;
 }
-unordered_map<int, int> hsh;
+gp_hash_table<int, int, chash> hsh;
 set<int> coords;
 st nums[200010];
 int bit[400010];
@@ -64,6 +96,7 @@ int ans1[200010];
 int ans2[200010];
 int main()
 {
+    input();
     int n, indx = 0;
     cin >> n;
     for(int i = 0; i < n; i ++)
