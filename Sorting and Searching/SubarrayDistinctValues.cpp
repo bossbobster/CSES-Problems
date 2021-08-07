6bb1fffd77b5597aef2fbdde856f98a659ace1cf
BossBobster <34829413+bossbobster@users.noreply.github.com>
2021-08-07 12:26:04 -0700

    Improved runtime.

--- Sorting and Searching/SubarrayDistinctValues.cpp
+++ Sorting and Searching/SubarrayDistinctValues.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string.h>
+#include <random>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iomanip>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
 #include <vector>
 #include <stack>
 #include <queue>
@@ -10,21 +17,48 @@
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
+#include <complex>
+#include <valarray>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
 typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<float, float> pff;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
 typedef pair<ll, ll> pll;
-#define max(n, m) ((n>m)?n:m)
-#define min(n, m) ((n<m)?n:m)
+typedef pair<int, ll> pil;
+typedef pair<ull, ull> pull;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
- 
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+struct chash
+{
+    const uint64_t C = ll(2e18*(3.14))+71;
+    const int RANDOM = rand();
+    ll operator()(ll x) const
+    {
+        return __builtin_bswap64((x^RANDOM)*C);
+    }
+};
+
 int nums[200010];
-set<int> vals;
-map<int, int> cnt;
+gp_hash_table<int, null_type, chash> vals;
+gp_hash_table<int, int, chash> cnt;
 int main()
 {
+    input();
     int n, k, idx = 0;
     ll ans = 0;
     cin >> n >> k;
