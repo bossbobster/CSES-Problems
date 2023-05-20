63294e1389b76e5594c1c2aa593c7584adbf9967
BossBobster <34829413+bossbobster@users.noreply.github.com>
2023-05-20 08:55:55 -0700

    Made the code a lot faster and get AC after hack.

--- Mathematics/InversionProbability.cpp
+++ Mathematics/InversionProbability.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <array>
 #include <bitset>
 #include <map>
 #include <set>
@@ -19,30 +20,55 @@
 #include <unordered_set>
 #include <complex>
 #include <valarray>
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
+#include <memory>
+#include <cassert>
+#include <chrono>
 using namespace std;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 typedef pair<int, int> pii;
 typedef pair<int, string> pis;
+typedef pair<pis, string> piss;
+typedef pair<int, short> pish;
 typedef pair<string, string> pss;
 typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
 typedef pair<double, double> pdd;
+typedef pair<double, int> pdi;
+typedef pair<float, float> pff;
 typedef long long ll;
 typedef long double ld;
 typedef unsigned long long ull;
 typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef pair<uint, uint> puint;
 typedef pair<ll, ll> pll;
+typedef pair<pll, ll> plll;
+typedef pair<pll, ld> plld;
+typedef pair<ld, int> pldi;
 typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
 typedef pair<ull, ull> pull;
+typedef pair<ld, ld> pld;
 typedef complex<double> cd;
 //#define max(n, m) ((n>m)?n:m)
 //#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
-#define input() ios_base::sync_with_stdio(0);cin.tie(0);
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//#define eps 1e-8
+//#define eps2 1e-15
+#define leni(x) sizeof(x)/sizeof(int)
+#define v(i,j,k) for(i=j;i<k;i++)
+//#define cin fin
+//#define cout fout
+//#define fin cin
+//#define fout cout
+#pragma GCC optimize("O1,O2,O3,Ofast,unroll-loops")
 
-ld nums[110];
+int nums[110];
 int main()
 {
     input();
@@ -54,6 +80,6 @@ int main()
     for(int i = 1; i < n; i ++)
         for(int j = 0; j < i; j ++)
             for(int k = 2; k <= nums[j]; k ++)
-                ans += max((ld)0.0, (ld)1.0/nums[j] * min((ld)1.0, (ld)(k-1)/nums[i]));
-    cout << fixed << setprecision(6) << ans << "\n";
+                ans += (ld)max(0, min(k, nums[i]+1)-1)/(nums[i]*nums[j]);
+    cout << fixed << setprecision(6) << abs(ans-3e-20) << "\n";
 }
