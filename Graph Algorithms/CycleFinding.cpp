eaca9a733e0a9a1f94385315464b7b0bf7b39ff1
BossBobster <34829413+bossbobster@users.noreply.github.com>
2023-07-25 10:34:15 -0700

    Fixed code for hack case

--- Graph Algorithms/CycleFinding.cpp
+++ Graph Algorithms/CycleFinding.cpp
@@ -1,28 +1,75 @@
 #include <iostream>
+#include <string.h>
+#include <random>
 #include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <iomanip>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
 #include <vector>
 #include <stack>
 #include <queue>
+#include <array>
 #include <bitset>
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
-//#include <ext/pb_ds/assoc_container.hpp>
-//using namespace __gnu_pbds;
+#include <complex>
+#include <valarray>
+#include <memory>
+#include <cassert>
+#include <chrono>
+#include <type_traits>
+#include <stdexcept>
+#include <string_view>
 using namespace std;
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+//template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 typedef pair<int, int> pii;
+typedef pair<int, string> pis;
+typedef pair<pis, string> piss;
+typedef pair<int, short> pish;
+typedef pair<string, string> pss;
+typedef pair<int, char> pic;
 typedef pair<pii, int> piii;
+typedef pair<double, double> pdd;
+typedef pair<double, int> pdi;
+typedef pair<float, float> pff;
 typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef unsigned short ushort;
+typedef pair<uint, uint> puint;
 typedef pair<ll, ll> pll;
-#define max(n, m) ((n>m)?n:m)
-#define min(n, m) ((n<m)?n:m)
+typedef pair<pll, ll> plll;
+typedef pair<pll, ld> plld;
+typedef pair<ld, int> pldi;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef pair<ull, ull> pull;
+typedef pair<ld, ld> pld;
+typedef complex<double> cd;
+//#define max(n, m) ((n>m)?n:m)
+//#define min(n, m) ((n<m)?n:m)
 #define f first
 #define s second
+#define input() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//#define eps 1e-8
+//#define eps2 1e-15
+#define leni(x) sizeof(x)/sizeof(int)
+#define v(i,j,k) for(i=j;i<k;i++)
+//#define cin fin
+//#define cout fout
+//#define fin cin
+//#define fout cout
+#pragma GCC optimize("O1,O2,O3,Ofast,unroll-loops")
 
 struct edge
 {
@@ -38,6 +85,7 @@ set<ll> in;
 unordered_map<ll, int> pos;
 int main()
 {
+    input();
     ll n, m, one, two, three, nd = 0, cur;
     cin >> n >> m;
     for(int i = 0; i <= n; i ++) dist[i] = 0;
@@ -52,7 +100,7 @@ int main()
         {
             if(dist[edges[j].s] + edges[j].c < dist[edges[j].e])
             {
-                if(i >= n - 1) { dist[n] = -2000000000000000000; nd = edges[j].s; break; }
+                if(i >= n - 1) { par[edges[j].e] = edges[j].s; dist[n] = -2000000000000000000; nd = edges[j].s; break; }
                 dist[edges[j].e] = dist[edges[j].s] + edges[j].c;
                 par[edges[j].e] = edges[j].s;
             }
